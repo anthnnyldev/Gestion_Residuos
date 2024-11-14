27b15312aeaa78e0e73314ac0492be2c396c4ec7
Batman <merchanbalcazaranderson@gmail.com>
2024-11-14 13:46:59 -0500

    urlss

--- app/core/home/urls.py
+++ app/core/home/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from core.home.views import home
+from core.home.views import home,NoticiasView, TutorialView, SobreNosotrosView, ContactoView, LoginView
 
 app_name = "home"
 
 urlpatterns = [
     #HOME
     path('',home.HomeView.as_view(),name="home"),
-]
+    path('noticias/', NoticiasView.as_view(), name='noticias'),
+    path('tutorial/', TutorialView.as_view(), name='tutorial'),
+    path('sobre-nosotros/', SobreNosotrosView.as_view(), name='sobre_nosotros'),
+    path('contacto/', ContactoView.as_view(), name='contacto'),
+    path('login/', LoginView.as_view(), name='login'),
--- app/core/home/views/home.py
+++ app/core/home/views/home.py
@@ -3,3 +3,18 @@ from django.views.generic import TemplateView, ListView
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
+
+class NoticiasView(TemplateView):
+    template_name = 'noticias.html'
+
+class TutorialView(TemplateView):
+    template_name = 'tutorial.html'
+
+class SobreNosotrosView(TemplateView):
+    template_name = 'sobre_nosotros.html'
+
+class ContactoView(TemplateView):
+    template_name = 'contacto.html'
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
