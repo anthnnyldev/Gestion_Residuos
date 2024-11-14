e574a5fe1546142074635ef82bb8aac157f60ffe
Batman <merchanbalcazaranderson@gmail.com>
2024-11-14 13:52:05 -0500

    cambio

--- app/core/home/urls.py
+++ app/core/home/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from core.home.views import home,NoticiasView, TutorialView, SobreNosotrosView, ContactoView, LoginView
+from core.home.views import home
 
 app_name = "home"
 
 urlpatterns = [
     #HOME
     path('',home.HomeView.as_view(),name="home"),
-    path('noticias/', NoticiasView.as_view(), name='noticias'),
-    path('tutorial/', TutorialView.as_view(), name='tutorial'),
-    path('sobre-nosotros/', SobreNosotrosView.as_view(), name='sobre_nosotros'),
-    path('contacto/', ContactoView.as_view(), name='contacto'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('noticias/', home.NoticiasView.as_view(), name='noticias'),
+    path('tutorial/', home.TutorialView.as_view(), name='tutorial'),
+    path('sobre-nosotros/', home.SobreNosotrosView.as_view(), name='sobre_nosotros'),
+    path('contacto/', home.ContactoView.as_view(), name='contacto'),
+    path('login/', home.LoginView.as_view(), name='login'),
+]
