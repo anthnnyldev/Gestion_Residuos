18cc481655dd29bd7c1cea7b4a371b6b041bda1d
anthnnyl <zanthonygg@gmail.com>
2024-11-14 13:33:47 -0500

    htmls

--- app/core/home/views/home.py
+++ app/core/home/views/home.py
@@ -2,4 +2,4 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView, ListView
 
 class HomeView(TemplateView):
-    template_name = 'components/home.html'
+    template_name = 'home/home.html'
