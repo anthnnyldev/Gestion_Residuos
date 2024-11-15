b75e8d159ef72d410ba60126a1c2ec32d7adea1d
Batman <merchanbalcazaranderson@gmail.com>
2024-11-15 13:34:42 -0500

    v4

--- app/core/home/views/home.py
+++ app/core/home/views/home.py
@@ -4,8 +4,5 @@ from django.views.generic import TemplateView, ListView
 class HomeView(TemplateView):
     template_name = 'home/home.html'   
 
-class ContactosView(TemplateView):
-    template_name = 'home/contact.html'
-
 class AboutView(TemplateView):
     template_name = 'home/about.html'
