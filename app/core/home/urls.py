1faaddb0484404375201be30eea2f5c44c7e7090
Batman <merchanbalcazaranderson@gmail.com>
2024-11-15 11:48:00 -0500

    graf

--- app/core/home/urls.py
+++ app/core/home/urls.py
@@ -7,8 +7,6 @@ app_name = "home"
 urlpatterns = [
     #HOME
     path('',home.HomeView.as_view(),name="home"),
-    path('noticias/', home.NewsView.as_view(), name='noticias'),
-    path('tutorial/', home.TutorialView.as_view(), name='tutorial'),
     path('about/', home.AboutView.as_view(), name='about'),
     path('contacto/', home.ContactosView.as_view(), name='contacto'),
     path('datos/', data.load_waste_data, name='datos'),
--- app/core/home/views/home.py
+++ app/core/home/views/home.py
@@ -2,14 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView, ListView
 
 class HomeView(TemplateView):
-    template_name = 'home/home.html'
-
-class NewsView(TemplateView):
-    template_name = 'home/news.html'
-
-class TutorialView(TemplateView):
-    template_name = 'home/tutorial.html'
-    
+    template_name = 'home/home.html'   
 
 class ContactosView(TemplateView):
     template_name = 'home/contact.html'
